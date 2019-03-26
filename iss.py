e5e03fce5de869dabb6e63abe3da39428c9e99fd
Jennifer Browning <browningjen@yahoo.com>
2019-03-25 21:15:32 -0400

    looks better

--- iss.py
+++ iss.py
@@ -5,81 +5,61 @@ __author__ = 'iam2angel' #Jen Browning
 import requests
 import turtle
 import time
+import argparse
+import sys
 
-def iss_astronauts():
+def get_astronauts():
     """Returns the full names, number, and spacecraft of astronauts in ISS."""
-    url = "http://api.open-notify.org/astros.json"
-    req = requests.get(url)
-    req_data = requests.json()
-
-    print('\nAstronauts in space')
-    print('Full Name and Craft Currently On')
-
-    for data in req_date['people']:
-        print('{} {}'.format(data['name'], data['craft']))
-
-    print('There is a current total of {} astronauts in space'
-          .format(str(req_data['number'])))
+    astro_data = requests.get("http://api.open-notify.org/astros.json")
+    return astro_data.json()
    
+   
+def get_coordinates():
+    coordinate_data = requests.get("http://api.open-notify.org/iss-now.json")
+    return coordinate_data.json()
+
+def indy_time():
+    coordinates = {'lat: 39.7684', 'lon: -861581'}
+    request = requests.get("http://api.open-notify.org/iss-pass.json", params=coordinates)
+    #time stamp
+    pass_time = time.ctime(request.json(['response'][0]['risetime']))
+    return pass_time
+
+def get_map()
+    data = get_coordinates()
+    pos = data['iss_position']
+    longitude = data['longitude']
+    latitude = data['latitude']
+    screen = turtle.Screen()
+    screen.bgpic('map.gif')
+    screen.setup(width=720, height=360, startx=None, starty=None)
+    screen.setworldcoordinates(-180, -90, 180, 90)
+    screen.register_shape('iss.gif')
+
+    iss = turtle.Turtle()
+    iss.setheading(90)
+    iss.penup()
+    iss.goto(float(longitude), float(latitude))
+
+    indy = turtle.Turtle()
+    indy.penup()
+    indy.color('green')
+    indy.goto(39.7684, -86.1581)
+    indy.dot(5)
+    indy.write(indy_time)
+    screen.exitonclick()
 
 
-
-
-# def get_coordinates():
-#     """Returns the current latitude and longitude with a time stamp"""
-#     #also need a time stamp for this function
-#     url = "http://api.open-notify.org/iss-now.json"
-#     res = requests.get(url)
-#     req_data = requests.json()
-#     longitude = req_data["iss_position"]["longitude"]
-#     latitude = req_data["iss_position"]["latitude"]
-#     timestamp = time.ctime(req_data["timestamp"])
-#     print("Longitude: {}.format(longitude)")
-#     print("Latitude: {}.format(latitude)")
-#     print("Timestamp: {}.format(timstamp)")
-#     return longitude, latitude, timestamp
-
-# def create_screen():
-#     screen = turtle.Screen()
-#     screen.setup(width=720, height=360, startx=None, starty=None)
-#     screen.bgpic('map.gif')
-#     screen.setworldcoordinates(-180, -90, 180, 90)
-#     return screen
-
-# def iss_map(screen, latitude, longitude):
-#     iss = turtle.Turle()
-#     screen.register_shape('iss.gif')
-#     iss.setheading(90)
-#     iss.penup()
-#     iss.goto(float(longitude), float(latitude))
-
-# def indy_pass(longitude, latitude):
-#     url = "http://api.open-notify.org/iss-pass.json"
-#     coord = {'lat: 39.7684', 'lon: -86.1581'}
-#     res = requests.get(url, params=coord)
-#     pass_time = time.ctime(res.json()['response'[0]['risetime']])
-#     return pass_time
-
-# def iss_dot(longitude, latitude, timestamp):
-#     indy = turtle.Turtle()
-#     indy.penup()
-#     indy.color('green')
-#     indy.goto(longitude,latitude)
-#     indy.dot(5)
-#     indy.hideturtle()
-#     indy.write(indy_pass(longitude, latitude))
-
-# def indy_coordinates():
-#     indy_latitude = 39.7684
-#     indy_longitude = -86.1581
-#     indy_time = indy_pass(indy_latitude, indy_longitude)
-#     iss_dot(indy_longitude, indy_latitude, indy_time)
-
-
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-a', '--astro', help='finds astronauts', action='store_true')
+    parser.add_argument('-c', '--coords', help='finds coordinates', action='store_true')
+    parser.add_argument('-m', '--map', help='displays map', action='store_true')
+    return parser
 
 
 def main():
-    iss_astronauts()
+    
 
 
 if __name__ == '__main__':
